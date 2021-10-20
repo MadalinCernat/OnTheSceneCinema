deafd1a4e894529e6cbbd0c25f1f724c4d5995d7
MadalinCernat <madalin.cernat02@gmail.com>
2021-10-20 23:04:58 +0300

    added main page design and appsettings content for it

--- OnTheSceneWebApp/Controllers/MoviesController.cs
+++ OnTheSceneWebApp/Controllers/MoviesController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using OnTheSceneWebApp.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,12 +12,22 @@ namespace OnTheSceneWebApp.Controllers
     public class MoviesController : Controller
     {
         private readonly ILogger<MoviesController> _logger;
+        private readonly IConfiguration _config;
 
-        public MoviesController(ILogger<MoviesController> logger)
+        public MoviesController(ILogger<MoviesController> logger, IConfiguration config)
         {
             _logger = logger;
+            _config = config;
         }
         public IActionResult Index()
+        {
+            var mainPageMovieOptions = new MainPageMovieOptions();
+
+            _config.GetSection("MainPageMovie").Bind(mainPageMovieOptions);
+
+            return View(mainPageMovieOptions);
+        }
+        public IActionResult List()
         {
             return View();
         }
---
+++ OnTheSceneWebApp/Models/MainPageMovieOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnTheSceneWebApp.Models
+{
+    public class MainPageMovieOptions
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string PosterUrl { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
